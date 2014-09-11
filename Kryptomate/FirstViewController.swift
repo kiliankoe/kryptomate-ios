8dd3d787dc7fd7bd543787add1e18440c04c3ece
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-09-12 01:19:05 +0200

    Initial Commit

---
+++ Kryptomate/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Kryptomate
+//
+//  Created by Kilian Koeltzsch on 12/09/14.
+//  Copyright (c) 2014 Kilian Koeltzsch. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Kryptomate/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Kryptomate
+//
+//  Created by Kilian Koeltzsch on 12/09/14.
+//  Copyright (c) 2014 Kilian Koeltzsch. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
